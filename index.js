d52b6a2944170b342dc40deed4d39fa7eac557c3
remarkable-workshop-2804 <jchaselubitz@gmail.com>
2018-11-14 22:07:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  
+  var x = {}
   Object.assign({x}, object, {[key]: value})
   return x
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object.key = value
+  return object
+}
+
+
 
 var obj = { prop: 1 }
 updateObjectWithKeyAndValue(obj, 'prop2', 2)
