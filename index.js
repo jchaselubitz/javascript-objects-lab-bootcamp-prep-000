4553b1a1a39b39f8579157ba9afb5f5e91a45488
remarkable-workshop-2804 <jchaselubitz@gmail.com>
2018-11-14 21:57:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ function updateObjectWithKeyAndValue(object, key, value) {
   var newObject = {}
   Object.assign({newObject}, object, {[key]: value})
   return newObject
-  return object
+
 }
+
+var obj = { prop: 1 }
+updateObjectWithKeyAndValue(obj, 'prop2', 2)
+
+console.log(updateObjectWithKeyAndValue())
